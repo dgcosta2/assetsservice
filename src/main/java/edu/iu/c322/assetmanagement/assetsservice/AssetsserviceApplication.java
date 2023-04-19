5e94ffd3a906fcde24f5b163ebb7ac3054add53c
Costa, Diego Souza Gomes da Cruz <dgcosta@iu.edu>
2023-04-19 17:27:29 -0400

    Initial commit

---
+++ src/main/java/edu/iu/c322/assetmanagement/assetsservice/AssetsserviceApplication.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.assetmanagement.assetsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/iu/c322/assetmanagement/assetsservice/AssetsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.assetmanagement.assetsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
