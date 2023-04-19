a2a2ea7af176ae4810b405cccf67b5f85e60bfc2
Costa, Diego Souza Gomes da Cruz <dgcosta@iu.edu>
2023-04-19 17:46:55 -0400

    asset

--- src/main/java/edu/iu/c322/assetmanagement/assetsservice/model/Asset.java
+++ src/main/java/edu/iu/c322/assetmanagement/assetsservice/model/Asset.java
@@ -1,9 +1,8 @@
 package edu.iu.c322.assetmanagement.assetsservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Objects;
 
 @Entity
 public class Asset {
@@ -12,6 +11,62 @@ public class Asset {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
+    private int licenseId;
+    @Transient
+    private String licenseType;
+    @Transient
+    private String licenseDescription;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLicenseId() {
+        return licenseId;
+    }
+
+    public void setLicenseId(int licenseId) {
+        this.licenseId = licenseId;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
+
+    public String getLicenseDescription() {
+        return licenseDescription;
+    }
+
+    public void setLicenseDescription(String licenseDescription) {
+        this.licenseDescription = licenseDescription;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Asset asset = (Asset) o;
+        return id == asset.id && licenseId == asset.licenseId && name.equals(asset.name) && licenseType.equals(asset.licenseType) && licenseDescription.equals(asset.licenseDescription);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, licenseId, licenseType, licenseDescription);
+    }
 }
